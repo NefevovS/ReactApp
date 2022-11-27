651439e30fd7c22bb00d609272d11f27338298d1
Sergey Nefedov <nefedov38@gmail.com>
2022-11-27 17:13:28 +0300

    add store

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,101 +1,108 @@
-import rerenderIntireTree from "./../../render";
-
-let state = {
-  profile: {
-    posts: [
-      {
-        id: 1,
-        message:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
-        likesCount: 10,
-      },
-      {
-        id: 2,
-        message:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
-        likesCount: 5,
-      },
-      {
-        id: 3,
-        message:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
-        likesCount: 15,
-      },
-    ],
-    newPostText: "",
+let store = {
+  _state: {
+    profile: {
+      posts: [
+        {
+          id: 1,
+          message:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
+          likesCount: 10,
+        },
+        {
+          id: 2,
+          message:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
+          likesCount: 5,
+        },
+        {
+          id: 3,
+          message:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis voluptate debitis error maiores exercitationem consequuntur qui adipisci neque quidem labore iusto, fugiat, sit at ratione natus molestiae aperiam quod?",
+          likesCount: 15,
+        },
+      ],
+      newPostText: "",
+    },
+    messages: {
+      allDialogs: [
+        {
+          name: "Sergey",
+          id: 1,
+        },
+        {
+          name: "Sveta",
+          id: 2,
+        },
+        {
+          name: "Natasha",
+          id: 3,
+        },
+        {
+          name: "Ivan",
+          id: 4,
+        },
+        {
+          name: "Igor",
+          id: 5,
+        },
+        {
+          name: "Alexander",
+          id: 6,
+        },
+      ],
+      allMessages: [
+        { imAuthor: true, message: "How are you?", id: 1 },
+        { imAuthor: false, message: "Ololo", id: 2 },
+        { imAuthor: false, message: "Упячка", id: 3 },
+        { imAuthor: true, message: "1243124", id: 4 },
+        { imAuthor: false, message: "ыалдтдйлцд", id: 5 },
+      ],
+      newMessage: "",
+    },
+    avatar: {
+      friends: [
+        {
+          id: 1,
+          name: "Sergey",
+          avatarSrc: "https://cdn.freelance.ru/images/att/1575043_900_600.png",
+        },
+        {
+          id: 2,
+          name: "Maria",
+          avatarSrc:
+            "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-48.jpg",
+        },
+      ],
+    },
   },
-  messages: {
-    allDialogs: [
-      {
-        name: "Sergey",
-        id: 1,
-      },
-      {
-        name: "Sveta",
-        id: 2,
-      },
-      {
-        name: "Natasha",
-        id: 3,
-      },
-      {
-        name: "Ivan",
-        id: 4,
-      },
-      {
-        name: "Igor",
-        id: 5,
-      },
-      {
-        name: "Alexander",
-        id: 6,
-      },
-    ],
-    allMessages: [
-      { imAuthor: true, message: "How are you?", id: 1 },
-      { imAuthor: false, message: "Ololo", id: 2 },
-      { imAuthor: false, message: "Упячка", id: 3 },
-      { imAuthor: true, message: "1243124", id: 4 },
-      { imAuthor: false, message: "ыалдтдйлцд", id: 5 },
-    ],
-    newMessage: "",
+  getState() {
+    return this._state;
   },
-  avatar: {
-    friends: [
-      {
-        id: 1,
-        name: "Sergey",
-        avatarSrc: "https://cdn.freelance.ru/images/att/1575043_900_600.png",
-      },
-      {
-        id: 2,
-        name: "Maria",
-        avatarSrc:
-          "https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-48.jpg",
-      },
-    ],
+  _rerenderIntireTree() {
+    console.log("ссылка на эту функцию меняется на ссылку observer");
+  },
+  addMsg(postMsg) {
+    let newpost = { id: 5, message: postMsg, likesCount: 0 };
+    this._state.profile.posts.push(newpost);
+    this._rerenderIntireTree(this._state);
+    this._state.profile.newPostText = "";
+  },
+  updateNewPostText(newText) {
+    this._state.profile.newPostText = newText;
+    this._rerenderIntireTree(this._state);
+  },
+  subscribe(observer) {
+    this._rerenderIntireTree = observer;
+  },
+  addMessage(newMessage) {
+    let msg = { imAuthor: true, message: newMessage, id: 10 };
+    this._state.messages.allMessages.push(msg);
+    this._rerenderIntireTree(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.messages.newMessage = newText;
+    this._rerenderIntireTree(this._state);
   },
 };
 
-export let addMsg = (postMsg) => {
-  let newpost = { id: 5, message: postMsg, likesCount: 0 };
-  state.profile.posts.push(newpost);
-  rerenderIntireTree(state);
-  state.profile.newPostText = "";
-};
-export let addMessage = (newMessage) => {
-  let msg = { imAuthor: true, message: newMessage, id: 10 };
-  state.messages.allMessages.push(msg);
-  rerenderIntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.profile.newPostText = newText;
-  rerenderIntireTree(state);
-};
-export let updateNewMessageText = (newText) => {
-  state.messages.newMessage = newText;
-  console.log(state.messages.newMessage);
-  rerenderIntireTree(state);
-};
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import rerenderIntireTree from "./render";
-import state from "./components/redux/state";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import store from "./components/redux/state";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-rerenderIntireTree(state);
+let rerenderIntireTree = () => {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={store.getState()}
+        addMsg={store.addMsg.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
+      />
+    </React.StrictMode>
+  );
+};
+rerenderIntireTree(store._state);
+store.subscribe(rerenderIntireTree);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
